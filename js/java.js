cf9b2447ab0a4488cf047a35fc1e0f8de10cce79
Adrian Wanjau <adrianhubba@gmail.com>
2020-02-21 14:17:29 +0300

    Added a working form

--- js/java.js
+++ js/java.js
@@ -1,69 +1,44 @@
-var CC = parseInt(prompt("Enter your First 2 digits of the Year you were born"));
-var YY = parseInt(prompt("Enter your Last 2 digits of the Year you were born"));
-var MM = parseInt(prompt("Enter your month number you were born"));
-var DD = parseInt(prompt("Enter your date you were born"));
-
-var dayOfWeek = function(YY, CC, MM, DD) {
-    return -1*(((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM+1) / 10)) + DD) % 7
-};
-var results = parseInt(dayOfWeek(YY, CC, MM, DD));
 var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+var akanMale = ["Kwasi", "Kwandwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var akanFemale = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
-var birthDay = days[results];
-if (birthDay=="undefined"){
-    alert(ERROR)
-}
-else{
-    alert("You were born on " + birthDay);
+function results(){
+    var YYYY = parseInt(document.getElementById("YYYY").value);
+    var MM = parseInt(document.getElementById("MM").value);
+    var DD = parseInt(document.getElementById("DD").value);
+    var birthDay = new Date(YYYY + "/" + MM + "/" + DD);
+    var dayOfWeek = birthDay.getDay();
+    var gender = print();
+    var akanName;
+
+    if(DD <1|| DD >31){
+        alert("Invalid Date");
+        return false;
+    }
+    if(MM <1|| MM >12){
+        alert("Invalid Month");
+        return false;
+    }
+    if(YYYY <1900|| YYYY >2100){
+        alert("Invalid Year");
+        return false;
+    }
+
+    if (gender === "Male") {
+        akanName = akanMale[dayOfWeek];        
+    }
+    else {
+        akanName = akanFemale[dayOfWeek]; 
+    }
+    alert("Your birth date was on a " +days[dayOfWeek]+ " and your akan name would be " +akanName+ ".");
 }
 
-var gender = (prompt("Please enter your Gender (Male or Female)"));
-if (gender == "Male") {
-    if (birthDay == "Sunday") {
-        alert("Akan name is Kwasi");
-    }
-    if (birthDay == "Monday") {
-        alert("Akan name is Kwandwo");
-    }
-    if (birthDay == "Tuesday") {
-        alert("Akan name is Kwabena");
-    }
-    if (birthDay == "Wednesday") {
-        alert("Akan name is Kwaku");
-    }
-    if (birthDay == "Thursday") {
-        alert("Akan name is Yaw");
-    }
-    if (birthDay == "Friday") {
-        alert("Akan name is Kofi");
-    }
-    if (birthDay == "Saturday") {
-        alert("Akan name is Kwame");
+function print(){
+    var gender = document.getElementsByName("choice");
+    for(i=0; i<gender.length; i++){
+        if (gender[i].checked){
+            return(gender[i].value)
+        }
     }
 }
-if (gender=="Female"){
-    if (birthDay == "Sunday") {
-        alert("Akan name is Akosua");
-    }
-    if (birthDay == "Monday") {
-        alert("Akan name is Adwoa");
-    }
-    if (birthDay == "Tuesday") {
-        alert("Akan name is Abenaa");
-    }
-    if (birthDay == "Wednesday") {
-        alert("Akan name is Akua");
-    }
-    if (birthDay == "Thursday") {
-        alert("Akan name is Yaa");
-    }
-    if (birthDay == "Friday") {
-        alert("Akan name is Afua");
-    }
-    if (birthDay == "Saturday") {
-        alert("Akan name is Ama");
-    }
-}
-else{
-    alert(ERROR)
-}
+
